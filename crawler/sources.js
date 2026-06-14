f6e17fe86ebf2f4b48432a642a5fc9c6c034863c
LANGTAOSHA-prog <taojiangtj@gmail.com>
2026-06-14 17:37:44 +0900

    Create sources.js

---
+++ crawler/sources.js
@@ -0,0 +1,9 @@
+export const SOURCES = [
+  "https://www.freeconvert.com/pdf-converter",
+  "https://www.freeconvert.com/video-converter",
+  "https://www.freeconvert.com/image-converter",
+  "https://www.freeconvert.com/audio-converter",
+  "https://www.freeconvert.com/document-converter",
+  "https://www.freeconvert.com/ebook-converter",
+  "https://www.freeconvert.com/compress"
+];
